85e830aa53434d13572f9ad5ad05057e90e7e1a8
tomo <tomo.pinjuh@gmail.com>
2016-11-11 11:05:19 +0100

    Prvi commit

---
+++ app/src/main/java/ba/fpmoz_sve_mo/pinjuh/testni/MainActivity.java
@@ -0,0 +1,13 @@
+package ba.fpmoz_sve_mo.pinjuh.testni;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
